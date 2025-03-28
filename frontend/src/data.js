722c081a764803bba9215192204ab8e10d593193
Jenna Chiarella <chiarellajenna@gmail.com>
2025-03-27 19:18:48 -0500

    Update data.js

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,8 +1,10 @@
 import titles from "./titles.json";
+import content from "./content.json";
 
 const sections = titles.map((book) => ({
     title: book.title, // Get title from JSON
-    content: "", // Content will be added later
+    content: content[book.id] || "content not available", // Content will be added later
 }));
 
 export default sections;
+
